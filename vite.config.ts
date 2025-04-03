3e31a796c6231901c5ba7032ff8b960e5b5c1a18
srjmalla <malla.srj@gmail.com>
2025-04-03 18:05:39 +0545

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      external: ['lucide-react'],
+      output: {
+        globals: {
+          'lucide-react': 'LucideReact'
+        }
+      }
+    }
+  }
+});
